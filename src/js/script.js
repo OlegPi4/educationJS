8fa4da7b8a58b7a53ca255f56049a4388233a057
OlegPichkur <oleg1pichkur@gmail.com>
2023-04-21 12:42:29 +0300

    lesson 1 only in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,19 @@
-'use strict';
 
-let indicatOne = 'yes',
-   indicatTwo = 'not';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(indicatOne || indicatTwo);
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов ?', ''),
+   b = prompt('На сколько оцените его ?', ''),
+   c = prompt('Один из последних просмотреных фильмов ?', ''),
+   d = prompt('На сколько оцените его ?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
