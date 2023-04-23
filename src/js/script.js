071604144063b62da407bb7ed352dac7568225f6
OlegPichkur <oleg1pichkur@gmail.com>
2023-04-23 22:53:01 +0300

    практика ч3 используем функции

--- src/js/script.js
+++ src/js/script.js
@@ -1,102 +1,66 @@
 'use strict';
 
-// let indicatOne = 'yes',
-//    indicatTwo = 'not';
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-// console.log(indicatOne || indicatTwo);
-// const personalMovieDB = {
-//    count: numberOfFilms,
-//    movies: {},
-//    actors: {},
-//    genres: [],
-//    privat: false
-// };
+function start() {
+   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// for (let i = 0; i < 2; i++) {
-//    const a = prompt('Один из последних просмотреных фильмов ?', ''),
-//       b = prompt('На сколько оцените его ?', '');
-   
-//    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//       personalMovieDB.movies[a] = b;
-//       console.log('done');
-//    } else {
-//       alert('Введите корректно!!');
-//       i--;
-//    }
-// }
-// let i = 0;
-// while (i < 2) {
-//    const a = prompt('Один из последних просмотреных фильмов ?', ''),
-//       b = prompt('На сколько оцените его ?', '');
-   
-//    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//       personalMovieDB.movies[a] = b;
-//       console.log('done');
-//       i++;
-//    } else {
-//       alert('Введите корректно!!');
+   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+      numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+   }
+}
+start();
+
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+function rememberMyFilms() {
+   for (let i = 0; i < 2; i++) {
+      const a = prompt('Один из последних просмотреных фильмов ?', ''),
+         b = prompt('На сколько оцените его ?', '');
       
-//    }
-// }
-
-
-// let numbSeen = personalMovieDB.count;
-
-// if (numbSeen < 10) {
-//    alert('Просмотрено мало фильмов');
-// } else if ( numbSeen >= 10 && numbSeen < 30) {
-//    alert('Вы класический зритель !');
-// } else if (numbSeen >= 30) {
-//    alert('Киноман ! ! !');
-// } else {
-//    alert('Произошла ошибка !');
-// }
-// console.log(personalMovieDB);
-
-// let num = 20;
-
-// function showInfo(text) {
-//    let num = text + 10;
-//    return num;
-// }
-
-// const messag = showInfo('Hello!  ');
-// console.log(messag);
-
-
-// function sayHello(firstName) {
-//    return 'Привет ' + firstName + '!';
-// }
-
-// console.log(sayHello('Антон'));
-
-// function formArray(a) {
-//    let array = [];
-//    array[0] = a - 1;
-//    array[1] = a;
-//    array[2] = a + 1;
-//    return array;
-// }
-
-// const outArr = formArray(5);
-// console.log(outArr);
-
-function getMathResult(numb, any) {
-   let result = '';
-   if (typeof (any) === 'string' || any <= 0) {
-      return result = +numb;
-   } else {
-      for (let i = 1; i <= any; i++) {
-         result = result + String(numb * i);
-         if (i < any) {
-            result = result + '---';
-         }
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMovieDB.movies[a] = b;
+      } else {
+         alert('Введите корректно!!');
+         i--;
       }
-      return result;
+   }
+   
+}
+rememberMyFilms();
+
+function detectPersonalLevel() {
+   let numbSeen = personalMovieDB.count;
+   if (numbSeen < 10) {
+      alert('Просмотрено мало фильмов');
+   } else if ( numbSeen >= 10 && numbSeen < 30) {
+      alert('Вы класический зритель !');
+   } else if (numbSeen >= 30) {
+      alert('Киноман ! ! !');
+   } else {
+      alert('Произошла ошибка !');
+   }
+} 
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+   if (!hidden) {
+      console.log(personalMovieDB);      
+   }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+   for (let i = 0; i <= 2; i++) {
+      personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`);
    }
 }
 
-const outMess = getMathResult(99, 4);
-console.log(outMess);
-console.log(`typeof outMess = ${typeof(outMess)}`);
+writeYourGenres();
+console.log(personalMovieDB);  
